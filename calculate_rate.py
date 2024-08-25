0f6b4828d2d7853523ed92322b05d31d186a250f
unknown <diloch111@gmail.com>
2024-08-25 04:07:58 +0100

    create module to calculate work rates and prompt user on performance

---
+++ calculate_rate.py
@@ -0,0 +1,19 @@
+def calculate_rate(real_time, real_qty, target_qty, task):
+    # assign task to items
+    if task == "case_pick":
+        item = "cases"
+    elif task == "rework":
+        item = "pallets"
+    # add more conditions as you scale use-cases
+    
+    # calculate the rates
+    real_rate = (60 * real_qty) / real_time
+    target_rate = target_qty
+    
+    if real_rate > target_rate:
+        return f"You are doing Excellent @ {real_rate} {item}/hr"
+    elif real_rate == target_rate:
+        return f"You are on Target @ {real_rate} {item}/hr"
+    else:
+        return f"You are below Target @ {real_rate} {item}/hr"
+        
